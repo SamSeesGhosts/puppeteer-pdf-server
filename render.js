baf0f6e133bb5d669f2e2dfc0243bd0646b6b44a
SamSeesGhosts <sj.christiansen22@gmail.com>
2025-07-09 18:05:23 -0400

    Update render.js

--- render.js
+++ render.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const puppeteer = require("puppeteer");
-const bodyParser = require("body-parser");
+import express from "express";
+import puppeteer from "puppeteer";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
